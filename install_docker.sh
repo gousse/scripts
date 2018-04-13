fecc09c16de55a843a88734c43e90c012385232f
gousse <bruno@bmunoz.net>
2018-04-13 18:39:51 +0200

    patch missing

--- install_docker.sh
+++ install_docker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # install docker
 echo "# install docker"
 ansible-playbook -i $DIR/../ansible/inventory/localhost $DIR/../ansible/docker_local.yml

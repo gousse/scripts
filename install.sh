9261d6a9aa31ad4e0c445933db58ede330a27408
Bruno Munoz <bruno.munoz@orange.com>
2018-04-19 17:50:16 +0200

    fix dir

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 sudo echo -n "" >/dev/null
 
 # manage proxy settings
-source proxy.sh
+source $DIR/proxy.sh
 
 # install apt packages
 bash $DIR/install_packages.sh

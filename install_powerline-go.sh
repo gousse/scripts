b65daa0a1197728d1f02042ff5e37ca3bf0d998b
Bruno Munoz <bruno.munoz@orange.com>
2018-04-19 14:28:33 +0200

    use my conf

--- install_powerline-go.sh
+++ install_powerline-go.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# install default
 sudo echo "" >/dev/null
 ansible-playbook -i $DIR/../ansible/inventory/localhost $DIR/../ansible/powerline-go.yml
 
-# copy specific config
-
+# use specific config
+rm -f $HOME/.config/powerline-go/config.sh
+POWERCONF=$(cd $DIR/../powerline-go && pwd)
+ln -s $POWERCONF/config.sh $HOME/.config/powerline-go/config.sh

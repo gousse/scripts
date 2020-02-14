91dfdb8afa940dce95aedf8130a9772b3c74c47a
Bruno Munoz <bruno.munoz@orange.com>
2020-02-14 10:44:19 +0100

    update install i3'

--- install_i3.sh
+++ install_i3.sh
@@ -2,11 +2,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
-sudo echo -n "" >/dev/null
-/usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2018.01.30_all.deb keyring.deb SHA256:baa43dbbd7232ea2b5444cae238d53bebb9d34601cc000e82f11111b1889078a
-sudo dpkg -i ./keyring.deb
-rm -f ./keyring.deb
-sudo bash -c "echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list"
+sudo bash -c "/usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2020.02.03_all.deb keyring.deb SHA256:c5dd35231930e3c8d6a9d9539c846023fe1a08e4b073ef0d2833acd815d80d48"
+sudo bash -c "dpkg -i ./keyring.deb"
+sudo bash -c "echo "deb https://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list"
 sudo apt update
 sudo apt install i3
 

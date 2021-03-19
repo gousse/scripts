5c22becd85bc80f6a549321e1251ab8670c0c740
Bruno Munoz <bruno.munoz@orange.com>
2021-03-19 09:41:18 +0100

    switch to python3

--- install_ansible.sh
+++ install_ansible.sh
@@ -3,9 +3,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "# install apt dependencies"
 sudo -E apt-get update
-sudo -E apt-get install -y build-essential libssl-dev libffi-dev python-dev python-minimal python-setuptools unzip gettext 
+#sudo -E apt-get install -y build-essential libssl-dev libffi-dev python-dev python-minimal python-setuptools unzip gettext 
+#sudo -E easy_install pip
 
-sudo -E easy_install pip
+sudo -E apt-get install -y build-essential libssl-dev libffi-dev python3-dev python3-minimal python3-setuptools python3-pip unzip gettext python-is-python3
+
 
 echo "# patching for ansible"
 envsubst < $DIR/../ansible/group_vars/all.template >  $DIR/../ansible/group_vars/all
@@ -13,7 +15,7 @@ envsubst < $DIR/../ansible/group_vars/all.template >  $DIR/../ansible/group_vars
 # install ansible
 echo "# install ansible"
 #sudo -E -H pip install pip ansible==2.4.3 --upgrade
-sudo -E -H pip install pip ansible --upgrade
+sudo -E -H pip3 install pip ansible --upgrade
 
 echo "# upgrade pyOpenSSL to the latest version (>=16)"
-sudo -E -H pip install pip pyOpenSSL --upgrade
+sudo -E -H pip3 install pip pyOpenSSL --upgrade

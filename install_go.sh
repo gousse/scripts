bae3ec3b9efae7609fe6f0cc2e3ab68c9a13e9c0
Bruno Munoz <bruno@bmunoz.net>
2023-01-25 16:05:59 +0100

    add env

--- install_go.sh
+++ install_go.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+VERSION="1.19.5"
 echo "# install go"
-ansible-playbook -i $DIR/../ansible/inventory/localhost $DIR/../ansible/go.yml
+
+curl -JLO "https://go.dev/dl/go${VERSION}.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go${VERSION}.linux-amd64.tar.gz
+
+
+echo " add to your ~/.profile"
+cat << EOF
+# set PATH for go
+if [ -d "/usr/local/go/bin" ] ; then
+  PATH="\$PATH:/usr/local/go/bin"
+fi
+
+if [ -d "\$HOME/go/bin" ] ; then
+  PATH="\$PATH:\$HOME/go/bin"
+fi
+EOF
